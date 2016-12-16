cf7f3ab170962d7006516d1a26ef4b5320238b6d
WangQingye <329103586@qq.com>
2016-12-16 19:50:28 +0800

    定义了新样式

--- js/main.js
+++ js/main.js
@@ -10,7 +10,9 @@
         $watch_detail,
         $task_detail = $('.task-detail'),
         $task_detail_mask = $('.task-detail-mask'),
-        task_list={}
+        task_list={},
+        $task_item,
+        $update_form
         ;
 
     init();
@@ -42,7 +44,7 @@
     });
 
 
-
+    //增加一条新事件
     function add_task(new_task){
         /*将新Task推入task_list*/
         task_list.push(new_task);
@@ -52,6 +54,7 @@
 
     }
 
+    //监听点击删除事件
     function listen_delete_task(){
 
         $delete_task.on('click',function(){   //事件嵌套是因为render之后才能找到删除键
@@ -71,6 +74,7 @@
         });
     }
 
+    //删除事件
     function delete_task(index){   /*删除task*/
 
         if(index === undefined || !task_list[index]) return;  //如果没写或者写了没有，就返回
@@ -81,7 +85,8 @@
 
     }
 
-    function listen_watch_detail(){
+    //监听点击查看详情事件
+    function listen_watch_detail(){    //事件嵌套是因为render之后才能找到删除键
 
         $watch_detail.on('click',function(){
 
@@ -91,21 +96,108 @@
 
             var index = $item.data('index');
 
-            console.log(index);
+            /*console.log(index);*/
 
             show_task_detail(index);
 
-        })
+        });
+
+        //双击事件查看
+        $task_item.on('dblclick',function(){
+
+            show_task_detail($(this).data('index'));
+
+        });
     }
 
+    //查看task详情：
     function show_task_detail(index){
+
+        render_task_detail(index);
 
         $task_detail.show();
         $task_detail_mask.show();
 
+        $task_detail_mask.on('click',function(){
+
+            $task_detail.fadeOut();
+            $task_detail_mask.fadeOut();
+
+        })
+
     }
 
-    function refresh_task_list(){   //更新localstorge中的task_list 并刷新
+    //更新任务详情:
+    function update_task_detail( index, data ){
+
+        if(index === undefined || !task_list[index]) return;
+
+        task_list[index] = data;
+
+        refresh_task_list();
+    }
+
+    //渲染特定的任务详情:
+    function render_task_detail(index){
+
+        if(index === undefined || !task_list[index]) return;
+
+        var item = task_list[index];
+
+       /* console.log(item);*/
+
+        var task_detail_tpl ='<form>'+
+            '<div class="content">'+item.content+'</div>'+
+            '<div><input  style="display:none" type="text" name="content" value="'+item.content+'"></div>'+
+            '<div>'+
+            '<div class="desc">'+
+            '<textarea name="desc">'+item.desc+'</textarea>'+
+            '</div>'+
+            '</div>'+
+            '<div class="remind">'+
+            '<input name="remind" type="date" value="'+item.remind_date+'">'+
+            '<button type="submit">更新</button>'+
+            '</div>'+
+            '</form>';
+
+        $task_detail.html(null);
+
+        $task_detail.html(task_detail_tpl);
+
+        $update_form = $task_detail.find('button');
+        var $detail_form = $task_detail.find('form');
+        var $update_form_content = $task_detail.find('.content');
+        var $update_form_content_input = $task_detail.find('[name=content]');
+
+        $update_form_content.on('dblclick',function(){
+            $update_form_content_input.show();
+            $update_form_content.hide();
+        });
+
+
+        $update_form.on('click',function(e){
+
+            e.preventDefault();
+
+            var data = {};
+
+            data.content = $detail_form.find('[name=content]').val();
+
+            data.desc = $detail_form.find('[name=desc]').val();
+
+            data.remind_date = $detail_form.find('[name=remind]').val();
+
+            /*console.log(data);*/
+            update_task_detail(index,data);
+
+            $task_detail.hide();
+            $task_detail_mask.hide();
+        })
+
+    }
+
+    //更新localstorge中的task_list 并刷新:
+    function refresh_task_list(){
 
         store.set('task_list',task_list);
 
@@ -113,7 +205,8 @@
 
     }
 
-    function init(){    /*初始化加载*/
+    /*初始化加载*/
+    function init(){
 
         task_list = store.get('task_list') || [];
 
@@ -121,7 +214,8 @@
 
     }
 
-    function render_task_list(){    /*渲染全部模板*/
+    /*渲染全部模板*/
+    function render_task_list(){
 
         var $task_list = $('.task-list');
 
@@ -137,11 +231,13 @@
 
        $delete_task = $ ('.action.delete');
        $watch_detail = $('.action.detail');
+       $task_item = $('.task-item');
        listen_delete_task();   //在此调用事件添加，才能成功添加
        listen_watch_detail();
 
     }
 
+    //生成一条任务
     function render_task_tpl(data,index){
 
         if(!data || !index ) return;  /*如果没有数据，就不添加*/
